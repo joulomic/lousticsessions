80f4ce7025b96628b6493ebb09776ab8a632fec4
joul <joulomic@hotmail.com>
2018-11-30 13:47:47 +0100

    test

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
-var app = express();
+    const express = require('express');
+    const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.listen((process.env.PORT || 3000));
+    const app = express();
+    const verifyWebhook = require('./verify-webhook');
 
-// Server frontpage
-app.get('/', function (req, res) {
-    res.send('This is TestBot Server');
-});
+    app.get('/', verifyWebhook);
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-// Facebook Webhook
-app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === 'lousticsessions') {
-        res.send(req.query['hub.challenge']);
-    } else {
-        res.send('Invalid verify token');
-    }
-});
+    app.listen(process.env.PORT || 3000);
